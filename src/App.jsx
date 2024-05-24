f303ebf3929cf3e5dcb858d11ceeab3f7df42099
Brandon Castillo <brandoncastillo.09@gmail.com>
2024-05-24 20:44:19 -0300

    mood defined

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,89 @@
-import { useState } from "react";
-import "./App.css";
+import { useEffect, useState } from "react";
+import "./styles/app.css";
+import { discos } from "./utils/discos";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [respuesta, setRespuesta] = useState(0);
+  const [album, setAlbum] = useState({ name: "", link: "" });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    let random;
+    const DISCOSXMOOD = discos;
+
+    random = Math.floor(Math.random() * DISCOSXMOOD[respuesta].length);
+    setAlbum(DISCOSXMOOD[respuesta][random]);
+  }
 
   return (
-    <>
-       <h1>Ruta</h1>
-    </>
+    <div className="all">
+      <h1>Mood Checker</h1>
+      <br />
+      <div className="consigna">
+        <p>
+          Como se encuentra hoy?:
+          <br />
+          1. feliz
+          <br />
+          2. triste
+          <br />
+          3. enojado
+          <br />
+          4. miedoso
+          <br />
+          5. ansioso
+          <br />
+          6. aburrido
+          <br />
+          7. extremo
+          <br />
+          8. calmado
+          <br />
+          9. confundido
+          <br />
+          10.pensativo
+        </p>
+      </div>
+      <br />
+      <div className="input">
+        <label>Ingrese una opción</label>
+        <br />
+        <br />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="number"
+            value={respuesta}
+            onChange={(e) => setRespuesta(e.target.value)}
+            placeholder="Ingrese una opción"
+            required
+            min={1}
+            max={10}
+            autoFocus
+          ></input>
+          <br />
+          <br />
+          <button>Enviar</button>
+        </form>
+      </div>
+      <br />
+      {album.link ? (
+        <div className="video">
+          <iframe
+            width="560"
+            height="315"
+            src={album.link}
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
   );
 }
 
---
+++ src/utils/discos.js
@@ -0,0 +1,150 @@
+export const discos = {
+  //feliz
+  1: [
+    {
+      name: "drive my car",
+      link: "https://www.youtube.com/embed/kfSQkZuIx84?si=NdDSjgqCAY4MwVSH",
+    },
+    {
+      name: "bittersweet",
+      link: "https://www.youtube.com/embed/1lyu1KKwC74?si=Vd48vEYmeaP3Kw_2",
+    },
+    {
+      name: "all star",
+      link: "https://www.youtube.com/embed/L_jWHffIx5E?si=LZu-MKIDxFESI0cj",
+    },
+  ],
+  //triste
+  2: [
+    {
+      name: "manal",
+      link: "https://www.youtube.com/watch?v=Hq-1TW5Nk7U&list=PL_tKhcwp1CMLKVxMrmIkYXpMPKY_1XYiv",
+    },
+    {
+      name: "volumen 3",
+      link: "https://youtu.be/BYuf1NiHrLk?si=H6SgacxYPrlIzR1N",
+    },
+    {
+      name: "otro dia en el planeta tierra",
+      link: "https://www.youtube.com/watch?v=g5v5nDbcN1U&pp=ygUpaW50b3hpY2Fkb3Mgb3RybyBkaWEgZW4gZWwgcGxhbmV0YSB0aWVycmE%3D",
+    },
+    {
+      name: "l.a. woman",
+      link: "https://youtu.be/91vU3kHtnoU?si=cwfFly1bFzQiseYe",
+    },
+    {
+      name: "core",
+      link: "https://youtu.be/hvzHum9457w?si=b4QyEsS8DQdeBuZ-",
+    },
+    {
+      name: "thank you",
+      link: "https://www.youtube.com/watch?v=cKCykk6u6rU&list=PLvIW4BTlPesGBCGmTEzuU4p73sLSPlao3",
+    },
+  ],
+  //enojado
+  3: [
+    {
+      name: "the great southern kill",
+      link: "https://youtu.be/3W0Gh-rFg70?si=5enES0jhLYg7YFGq",
+    },
+    {
+      name: "grotesque impalement",
+      link: "https://youtu.be/zwYsHIaZPNE?si=6pq7lBnDGZLg29VU",
+    },
+    {
+      name: "mouth of war",
+      link: "https://www.youtube.com/watch?v=FTOilfxhwxs",
+    },
+  ],
+  //con miedo
+  4: [
+    {
+      name: "shed",
+      link: "https://youtu.be/vbeJ4-OMorU?si=IAEMK2wy-DL6amvZ",
+    },
+    {
+      name: "the first four years",
+      link: "https://youtu.be/z3QBw7WKRiU?si=GUCX-UtBIfOJ2Va0",
+    },
+    {
+      name: "los compadres",
+      link: "https://youtu.be/QSxXQ38AeJo?si=lxnY5LomNRZTtqe9",
+    },
+  ],
+  //con ansiedad
+  5: [
+    {
+      name: "flowers",
+      link: "https://www.youtube.com/watch?v=Ph3Raj7Jyk0&pp=ygUWZ2FsYXhpZSA1MDAgZnVsbCBhbGJ1bQ%3D%3D",
+    },
+    {
+      name: "on fire",
+      link: "https://youtu.be/vsfvdGjL4Yo?si=ZeqObXDkOXsJoRxO",
+    },
+    {
+      name: "souvlaki",
+      link: "https://youtu.be/HpzZ_sslsHM?si=CO3JjI52TFMs0UhH",
+    },
+  ],
+  //aburrido
+  6: [
+    {
+      name: "select ambient works 85-92",
+      link: "https://youtu.be/jhtxDbL15Q4?si=9NE3mEjrbgxIIyCS",
+    },
+    {
+      name: "let go",
+      link: "https://youtu.be/xlIgAhvjy_Y?si=eNHIa6ICWW_dcea4",
+    },
+  ],
+  //extremo
+  7: [
+    {
+      name: "break stuff",
+      link: "https://www.youtube.com/embed/ZpUYjpKg9KY?si=42YFJhB9r2m6dP6E",
+    },
+    {
+      name: "around the fur",
+      link: "https://youtu.be/qIb-4-AD-4U?si=wyWT8xohfwac6P8I",
+    },
+    {
+      name: "betty",
+      link: "https://youtu.be/Ymqgf0PJQ3U?si=_B29zuZu7emM70U2",
+    },
+    { name: "jar", link: "https://youtu.be/jhtxDbL15Q4?si=9NE3mEjrbgxIIyCS" },
+    {
+      name: "white pony",
+      link: "https://youtu.be/v1npHeOskdQ?si=BvzuxAEt6K_L1uZa",
+    },
+  ],
+  //calmado
+  8: [
+    { name: "loveless", link: "https://www.youtube.com/watch?v=bRtGBlOVLCk" },
+    {
+      name: "fresh air",
+      link: "https://www.youtube.com/watch?v=ps8D1Fu-K8E",
+    },
+  ],
+  //confundido
+  9: [
+    {
+      name: "beach music",
+      link: "https://www.youtube.com/watch?v=whcj9VHBLjU&list=PLZqsyBiYZFQ3jhJRkYMt5RH5R96tYBRf3",
+    },
+    {
+      name: "dots and loops",
+      link: "https://youtu.be/vbeJ4-OMorU?si=IAEMK2wy-DL6amvZ",
+    },
+    {
+      name: "they spent their wildful years in the saloon",
+      link: "https://youtu.be/S1rTKIsDS8o?si=Xl7N9X_myV07HEjl",
+    },
+  ],
+  //pensativo
+  10: [
+    {
+      name: "druk qw",
+      link: "https://youtu.be/u3UhZfJElek?si=53b7H7BdcVMmI5b-",
+    },
+  ],
+};
