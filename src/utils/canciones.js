4a408550f2244946d7e21ce56f316fe741714b54
Brandon Castillo <brandoncastillo.09@gmail.com>
2025-08-19 11:08:05 -0300

    UPDATE: canciones

--- src/utils/canciones.js
+++ src/utils/canciones.js
@@ -21,13 +21,21 @@ export const discos = {
   //triste
   2: [
     {
-      name: "she foudn",
+      name: "she found now",
       link: "rBKjhgHGVZs",
     },
     {
       name: "riot van",
       link: "a62FZZa3fjE",
     },
+    {
+      name: "dagger",
+      link: "NYsX51qP40o"
+    },
+    {
+      name: "the lonely sea",
+      link: "06B_1t6-WKM"
+    }
   ],
   //enojado
   3: [
@@ -35,6 +43,22 @@ export const discos = {
       name: "dead and bloaded",
       link: "SCx_uiSdC_c",
     },
+    {
+      name: "break stuff",
+      link: "ZpUYjpKg9KY"
+    },
+    {
+      name: "engine no 9",
+      link: "OC8x9_App2I"
+    },
+    {
+      name: "7 words",
+      link: "mEVik1nJb68"
+    },
+    {
+      name: "my name is mud",
+      link: "953PkxFNiko"
+    }
   ],
   //con miedo
   4: [
@@ -42,6 +66,14 @@ export const discos = {
       name: "27",
       link: "SCx_uiSdC_c",
     },
+    {
+      name: "the world is yours",
+      link: "e5PnuIRnJW8"
+    },
+    {
+      name: "life is a bitch",
+      link: "AxKleuLBRcw"
+    }
   ],
   //con ansiedad
   5: [
@@ -49,7 +81,14 @@ export const discos = {
       name: "dancefloor",
       link: "pK7egZaT3hs",
     },
-
+    {
+      name: "the world is a guetto",
+      link: "dkVLHl63lUE"
+    },
+    {
+      name: "complicated",
+      link: "5NPBIwQyPWE"
+    }
   ],
   //aburrido
   6: [
@@ -64,22 +103,45 @@ export const discos = {
     {
       name: "title fight - stab",
       link: "B3E4Ig1DWek"
+    },
+    {
+      name: "flim",
+      link: "-yNHlKAzyVA"
+    },
+    {
+      name: "xtal",
+      link: "uXpKC8TIAxE"
     }
-
   ],
   //eufórico
   7: [
     {
-      name: "break stuff",
-      link: "ZpUYjpKg9KY"
+      name: "rollin",
+      link: "RYnFIRc0k6E"
+    },
+    {
+      name: "my own summer",
+      "link": "XOzs1FehYOA&"
     },
     {
       name: "all star",
       link: "L_jWHffIx5E"
+    },
+    {
+      name: "pulsewidth",
+      link: "TfInTQz0kdM"
     }
   ],
   //calmado:
   8:[
+    {
+      name: "altogether",
+      link: "suEY0WcOsxJg",
+    },
+    {
+      name: "teeanger",
+      link: "rn2OM546an8"
+    },
     {
       name: "strewberry",
       link: "HtUH9z_Oey8",
@@ -87,16 +149,40 @@ export const discos = {
   ],
   //confundido
   9:[
-      {
-      name: "strewberry",
-      link: "HtUH9z_Oey8",
+    {
+      name: "a day in the life",
+      link: "usNsCeOV4GM",
+    },  
+    {
+      name: "stone in focus",
+      link: "q86g1aop6a8",
     },
   ],
   //reflexivo
   10:[
-      {
-      name: "strewberry",
-      link: "HtUH9z_Oey8",
+    {
+      name: "Rhubarb",
+      link: "_AWIqXzvX",
     },
+    {
+      name: "across the universe",
+      link: "we0tO0LxY8Y",
+    },
+    {
+      name: "14th avril",
+      link: "uxTdTaNIUxo"
+    }
   ]
 }
+
+
+// feliz
+// reflex
+// calmado
+// euforía
+// confundido
+// ansioso
+// enojado
+// triste
+// miedo
+// aburrido
